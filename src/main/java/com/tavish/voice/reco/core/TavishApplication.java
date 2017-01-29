52721c66746bbc87601cc9d2ffaf5ad48f272369
Subash Chandrabose, Karunakaran <karunakaran.subashchandrabose@astrazeneca.com>
2017-01-29 20:12:54 +0530

    Init Checkin

---
+++ src/main/java/com/tavish/voice/reco/core/TavishApplication.java
@@ -0,0 +1,12 @@
+package com.tavish.voice.reco.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TavishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TavishApplication.class, args);
+	}
+}
---
+++ src/test/java/com/tavish/voice/reco/core/TavishApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tavish.voice.reco.core;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TavishApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
